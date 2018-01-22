f4aba1cce792efce60abbfd0b512115238c35117
Ian Otto <ianotto24@gmail.com>
2018-01-22 16:02:17 -0700

    use the room functionality of socket

--- js/alertbox-control.js
+++ js/alertbox-control.js
@@ -1,11 +1,15 @@
 var socket = io("https://gardenbox.duper51.me", {path: "/api/socket.io"});
-socket.on('connect', function () {console.log("connected")});
+socket.on('connect', function (socket) {
+    console.log("connected");
+    socket.join(listenTo);
+    console.log("connected to room");
+});
 socket.on('event', function (data) {console.log(data)});
 socket.on('disconnect', function () {console.log("disconnected")});
 if(listenTo === undefined) {
     console.error("listenTo undefined");
 }
-socket.on(listenTo, function (data) {
+socket.on("payment", function (data) {
    if(data.from !== undefined && data.amount !== undefined) {
        $("whodonated").text(data.from);
        $("howmuch").text(data.amount);
