706f94aefea1354da858da801e39bb0ad2a2fe09
duper51 <ianotto24@gmail.com>
2018-01-22 19:51:15 -0700

    make alertbox socket connect to the correct room

--- js/alertbox-control.js
+++ js/alertbox-control.js
@@ -1,14 +1,20 @@
-var socket = io("https://gardenbox.duper51.me", {path: "/api/socket.io"});
-socket.on('connect', function (socket) {
+var socket = io("https://gardenbox.duper51.me", {
+    path: "/api/socket.io",
+    query: {
+        roomName: listenTo
+    }
+});
+socket.on('connect', function () {
     console.log("connected");
-    socket.join(listenTo);
-    console.log("connected to room");
 });
 socket.on('event', function (data) {console.log(data)});
 socket.on('disconnect', function () {console.log("disconnected")});
 if(listenTo === undefined) {
     console.error("listenTo undefined");
 }
+socket.on("svrerror", function (data) {
+    console.error(data);
+});
 socket.on("payment", function (data) {
    if(data.from !== undefined && data.amount !== undefined) {
        $("whodonated").text(data.from);
